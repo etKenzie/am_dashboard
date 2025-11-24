fd1d02566c3def5021acf91fc0e9f9112feea5f3
KenzieEvan <etkenzie@gmail.com>
2025-11-24 13:18:27 +0700

    remove exec dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 const nextConfig = { 
   reactStrictMode: false, 
   images: { unoptimized: true },
-  // Dynamic base path configuration
-  basePath: process.env.NODE_ENV === 'production' ? '/executive_dashboard' : '',
-  // Asset prefix for production
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/executive_dashboard' : '',
   // Trailing slash for consistency
   trailingSlash: false,
 };
